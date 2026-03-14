9c264db64c7102ce644b4b63afdc0f3704fcda85
Michael Deryugin <Michael.Deryugin@gmail.com>
2026-03-14 20:07:12 +0000

    implement vms start command

--- commands/start.sh
+++ commands/start.sh
@@ -3,5 +3,14 @@
 name="${1:-}"
 [[ -z "$name" ]] && die "usage: vms start <name>"
 
+if ! virsh dominfo "$name" &>/dev/null; then
+    die "VM '$name' does not exist"
+fi
+
+state=$(virsh domstate "$name" 2>/dev/null)
+if [[ "$state" == "running" ]]; then
+    die "VM '$name' is already running"
+fi
+
 info "Starting VM '$name'"
-echo "[TODO] virsh start $name"
+virsh start "$name"
---
+++ spec/commands/start_spec.sh
@@ -0,0 +1,45 @@
+Describe "vms start"
+  Include lib/common.sh
+
+  virsh() {
+    case "$1" in
+      dominfo)
+        [[ "$2" == "myvm" ]] && return 0
+        return 1
+        ;;
+      domstate)
+        echo "${_vm_state:-shut off}"
+        ;;
+      start)
+        echo "started $2"
+        return 0
+        ;;
+    esac
+  }
+
+  It "fails without arguments"
+    When run source commands/start.sh
+    The status should eq 1
+    The stderr should include "usage: vms start"
+  End
+
+  It "fails when VM does not exist"
+    When run source commands/start.sh "novm"
+    The status should eq 1
+    The stderr should include "does not exist"
+  End
+
+  It "fails when VM is already running"
+    _vm_state="running"
+    When run source commands/start.sh "myvm"
+    The status should eq 1
+    The stderr should include "already running"
+  End
+
+  It "starts a stopped VM"
+    _vm_state="shut off"
+    When run source commands/start.sh "myvm"
+    The status should eq 0
+    The output should include "started myvm"
+  End
+End
